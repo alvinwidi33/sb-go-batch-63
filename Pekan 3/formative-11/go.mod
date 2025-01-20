2b088168cc9eac89004f54ffb6c20abb1c04e7d5
Alvin Widi Nugroho <alvin.widi@ui.ac.id>
2025-01-20 17:03:17 +0700

    tugas-11 finished

---
+++ Pekan 3/formative-11/go.mod
@@ -0,0 +1,3 @@
+module formative-11
+
+go 1.23.5
---
+++ Pekan 3/formative-11/web-server/formative11.go
@@ -0,0 +1,29 @@
+package webserver
+
+import (
+	"fmt"
+	"math"
+	"net/http"
+)
+
+func luasAlas(jariJari int) string {
+	return fmt.Sprintf("luas alas: %.2f", math.Pi*math.Pow(float64(jariJari), 2))
+}
+
+func kelilingAlas(jariJari int) string {
+	return fmt.Sprintf("keliling alas: %.2f", math.Pi*2*float64(jariJari))
+}
+
+func volume(jariJari int, tinggi int) string {
+	return fmt.Sprintf("volume: %.2f", math.Pi*math.Pow(float64(jariJari), 2)*float64(tinggi))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	jariJari := 7
+	tinggi := 10
+
+	fmt.Fprintf(w, "jariJari: %d, tinggi: %d, volume: %s, luas alas: %s, keliling alas: %s\n",
+		jariJari, tinggi, volume(jariJari, tinggi), luasAlas(jariJari), kelilingAlas(jariJari))
+}
+
+
